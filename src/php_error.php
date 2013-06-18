0aac492af7898a49f8c4c6f4b5acc18876a9ae38
magobaol <francesco.face@gmail.com>
2013-06-18 14:57:44 +0200

    Added mapping of PHP Errors, mapped to descriptions of them

--- src/php_error.php
+++ src/php_error.php
@@ -525,6 +525,29 @@
             );
 
             /**
+             * A mapping of PHP errors,
+             * mapped to descriptions of them.
+             */
+            private static $PHP_ERROR_MAPPINGS = array(
+		            		E_ERROR 											=> 'E_ERROR',
+										E_WARNING 										=> 'E_WARNING',
+										E_PARSE 											=> 'E_PARSE',
+										E_NOTICE 											=> 'E_NOTICE',
+										E_CORE_ERROR 									=> 'E_CORE_ERROR',
+										E_CORE_WARNING 								=> 'E_CORE_WARNING',
+										E_COMPILE_ERROR 							=> 'E_COMPILE_ERROR',
+										E_COMPILE_WARNING 					  => 'E_COMPILE_WARNING',
+										E_USER_ERROR									=> 'E_USER_ERROR',
+										E_USER_WARNING 								=> 'E_USER_WARNING',
+										E_USER_NOTICE 								=> 'E_USER_NOTICE',
+										E_STRICT 											=> 'E_STRICT',
+										E_RECOVERABLE_ERROR 					=> 'E_RECOVERABLE_ERROR',
+										E_DEPRECATED 									=> 'E_DEPRECATED',
+										E_USER_DEPRECATED 						=> 'E_USER_DEPRECATED',
+										E_ALL 												=> 'E_ALL'
+						);
+
+            /**
              * A list of methods which are known to call the autoloader,
              * but should not error, if the class is not found.
              *
